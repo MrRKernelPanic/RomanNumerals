6eb7f9fd5f85bbb36a15e625982c12c01213f373
Mark Routledge <mark@mroutledge.co.uk>
2021-07-22 23:19:05 +0100

    Converts numbers 1 to 8 into roman numerals handling V for 5

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,10 +1,19 @@
 function arabic_to_roman_numerals(num) {
     var roman_numerals = ''
     
-    for (var i = 1; i <=num ; i++) {
-        roman_numerals += 'I';
+    if (Math.floor(num/5) == 1) {
+        roman_numerals += 'V'
     }
-      
+
+    if (num % 5 <= 3) {
+        for (var i = 1; i <= num % 5 ; i++) {
+            roman_numerals += 'I';
+        }
+    }
+    if (num % 5 == 4) {
+        roman_numerals += 'IV';
+    }    
+
     return roman_numerals;
 }
 module.exports = arabic_to_roman_numerals;
--- roman_numerals.test.js
+++ roman_numerals.test.js
@@ -12,4 +12,20 @@ describe('Arabic to Roman', () => {
     test('3 in Roman Numerals is III', () => {
         expect(arabic_to_roman_numerals(3)).toBe('III');
     });
+
+    test('4 in Roman Numerals is IV', () => {
+        expect(arabic_to_roman_numerals(4)).toBe('IV');
+    });
+
+    test('5 in Roman Numerals is V', () => {
+        expect(arabic_to_roman_numerals(5)).toBe('V');
+    });
+
+    test('6 in Roman Numerals is VI', () => {
+        expect(arabic_to_roman_numerals(6)).toBe('VI');
+    });
+
+    test('8 in Roman Numerals is VIII', () => {
+        expect(arabic_to_roman_numerals(8)).toBe('VIII');
+    });
 });
