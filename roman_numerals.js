08bd96e14aa254c7b8f0bbb0d02d26be3379890f
Mark Routledge <mark@mroutledge.co.uk>
2021-07-26 00:01:48 +0100

    Refactored to use Dictionary and cycle throught Thousands, Hundreds and Tens

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,52 +1,36 @@
 function arabic_to_roman_numerals(num) {
     var roman_numerals = '';
+    let dict= {
+        1000 : 'M',
+        500 : 'D',
+        100 : 'C',
+        50 : 'L',
+        10 : 'X',
+        5 : 'V',
+        1 : 'I'
+       }
+    
+    const THT = Object.keys(dict);
+    THT.reverse();
+    for (let i = 0; i < 5; i+=2) {
 
-    while(Math.floor(num/1000) <= 4 && Math.floor(num/1000) != 0) {
-        roman_numerals += 'M';
-        num -= 1000;
-    }
-    if (num % 1000 >= 900) {
-        roman_numerals +='CM';
-        num -= 900;
-    }
+        while(Math.floor(num/THT[i]) <= 4 && Math.floor(num/THT[i]) != 0) {
+            roman_numerals += dict[THT[i]];
+            num -= THT[i];
+        }
+        if (num % THT[i] >= (THT[i]-THT[i+2])) {
+            roman_numerals += dict[THT[i+2]] + dict[THT[i]];
+            num -= (THT[i]-THT[i+2]);
+        }
 
-    if (Math.floor(num/500) == 1) {
-        roman_numerals += 'D';
-        num -=500;
-    }
-    if (num % 500 >= 400) {
-        roman_numerals +='CD';
-        num -= 400;
-    }
-
-    while(Math.floor(num/100) <= 3 && Math.floor(num/100) != 0) {
-        roman_numerals += 'C';
-        num -= 100;
-    }
-
-    if (num % 100 >= 90) {
-        roman_numerals +='XC';
-        num -= 90;
-    }
-
-    if (Math.floor(num/50) == 1) {
-        roman_numerals += 'L';
-        num -= 50;
-    }
-
-    if (num%50 >= 40) {
-        roman_numerals += 'XL';
-        num -= 40;
-    }
-
-    while(Math.floor(num/10) <= 3 && Math.floor(num/10) != 0) {
-        roman_numerals += 'X';
-        num -= 10;
-    }
-
-    if (num % 10 == 9) {
-        roman_numerals +='IX';
-        num -= 9;
+        if (Math.floor(num/THT[i+1]) == 1) {
+            roman_numerals += dict[THT[i+1]];
+            num -= THT[i+1];
+        }
+        if (num % THT[i+1] >= (THT[i+1] - THT[i+2])) {
+            roman_numerals += dict[THT[i+2]] + dict[THT[i+1]];
+            num -= THT[i+1] - THT[i+2];
+        }
     }
 
     if (Math.floor(num/5) == 1) {
