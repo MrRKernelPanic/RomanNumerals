e21631807531d23481118152d276a6bc7de22255
Mark Routledge <mark@mroutledge.co.uk>
2021-07-26 21:53:55 +0100

    Refactoring and simplification of code

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,50 +1,29 @@
-function arabic_to_roman_numerals(num) {
+function arabic_to_roman_numerals(arabic_number) {
     var roman_numerals = '';
-    let dict= {
-        1000 : 'M',
-        500 : 'D',
-        100 : 'C',
-        50 : 'L',
-        10 : 'X',
-        5 : 'V',
-        1 : 'I'
-       }
+    var roman_numeral_lookups = {
+        '1000' : 'M',
+        '900' : 'CM',
+        '500' : 'D',
+        '400' : 'CD',
+        '100' : 'C',
+        '90' : 'XC',
+        '50' : 'L',
+        '40' : 'XL', 
+        '10' : 'X',
+        '9' : 'IX',
+        '5' : 'V',
+        '4' : 'IV',
+        '1' : 'I'
+    }
     
-    const THT = Object.keys(dict);
-    THT.reverse();
-    for (let i = 0; i < 5; i+=2) {
-
-        while(Math.floor(num/THT[i]) <= 4 && Math.floor(num/THT[i]) != 0) {
-            roman_numerals += dict[THT[i]];
-            num -= THT[i];
+    var numeral_values = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+    
+    numeral_values.forEach (function(value) {
+        while(arabic_number - value >= 0){
+            roman_numerals += roman_numeral_lookups[value];
+            arabic_number -= value;
         }
-        if (num % THT[i] >= (THT[i]-THT[i+2])) {
-            roman_numerals += dict[THT[i+2]] + dict[THT[i]];
-            num -= (THT[i]-THT[i+2]);
-        }
-
-        if (Math.floor(num/THT[i+1]) == 1) {
-            roman_numerals += dict[THT[i+1]];
-            num -= THT[i+1];
-        }
-        if (num % THT[i+1] >= (THT[i+1] - THT[i+2])) {
-            roman_numerals += dict[THT[i+2]] + dict[THT[i+1]];
-            num -= THT[i+1] - THT[i+2];
-        }
-    }
-
-    if (Math.floor(num/5) == 1) {
-        roman_numerals += 'V';
-    }
-
-    if (num % 5 <= 3) {
-        for (var i = 1; i <= num % 5 ; i++) {
-            roman_numerals += 'I';
-        }
-    }
-    if (num % 5 == 4) {
-        roman_numerals += 'IV';
-    }    
+    });
 
     return roman_numerals;
 }
